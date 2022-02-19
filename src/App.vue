32cc7bf6615254c5c6436b5aff9a1cc1f0245268
杨绅轩 <957355473@qq.com>
2022-02-19 16:53:53 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Frank />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Frank from "./components/Frank.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Frank,
+  },
+};
+</script>
